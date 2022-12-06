649226fef067b1b52d6fba72224b78cc79e40686
maria <ericapolo10@hotmail.com>
2022-12-06 08:40:31 -0500

    ok

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(morgan('dev'));
+//app.use(express);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
